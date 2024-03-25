3b2690633eb5e2f99676f79546e8da3400953650
Gokul-nath-K <gokulnathk003@gmail.com>
2024-03-26 00:40:36 +0530

    DEV 04 : Default Admin ID
    At time of first request new default admin credentials is added to the database.

--- SpringSecurity/src/main/java/FullMetal/SpringSecurity/SpringSecurityApplication.java
+++ SpringSecurity/src/main/java/FullMetal/SpringSecurity/SpringSecurityApplication.java
@@ -1,13 +1,28 @@
 package FullMetal.SpringSecurity;
 
+import FullMetal.SpringSecurity.DTO.Request.RegisterRequest;
+import FullMetal.SpringSecurity.Services.AuthenticationService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(AuthenticationService authenticationService) {
+		return args -> {
+			var admin = RegisterRequest.builder()
+					.name("Admin")
+					.email("admin@gmail.com").password("Admin@123")
+					.role("ADMIN")
+					.build();
+		};
 	}
 
 }
